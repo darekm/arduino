fe3f4fb6838f94be1b19e5a00141aa4369214a8d
Darek <darekm@emadar.com>
2015-05-03 21:17:18 +0200

    class Frame

--- libraries/metium/me_frame.h
+++ libraries/metium/me_frame.h
@@ -11,27 +11,35 @@
 
 #ifndef __MEFRAME_H__
 #define __MEFRAME_H__
+#include "Arduino.h"
 
-
-struct MemsValue_t {                  // Structure of our payload
+typedef struct  {                  // Structure of our payload
    byte action;
    byte address;
    word time;
    word value[3] ;
 
   unsigned long ms;
+}MemsValue_t;
+
+class FrameStream :  public Stream{
+};
+class Frame {
+    private:
+    Stream * _stream;
+    static const int _buffer_max = 128;
+    static char _buffer[_buffer_max];
+    unsigned char _yp, _ym, _xm, _xp;
+    public:
+    void setStream( Stream & s);
+    String ToFrame(MemsValue_t& t);
+    void write(MemsValue_t& t);
+    String ToString(MemsValue_t &t);
+    int FromFrame(String& s,MemsValue_t &t);
 };
 
-String ToString( MemsValue_t * t){
- String m="values::" ;
-   for(int i=0;i<3;i++){
-      m +="   v:";
-      m.concat(String(i));
-      m+="=";
-      m.concat(String ((*t).value[i]));
-    }
 
-  return m;
-}
+
+
 
 #endif // __MEFRAME_H__

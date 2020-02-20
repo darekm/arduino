e000d35309a35cfa67c2a48983e52fddd7f61bb9
darekm <darekm@emadar.com>
2020-02-20 15:51:25 +0100

    modbud485: initial implementation

---
+++ modbus485/modbus485.h
@@ -0,0 +1,117 @@
+// 
+//    FILE:
+// VERSION: 0.9.00
+// PURPOSE: modbus controller logger  for imwave
+//
+//
+// HISTORY:
+//
+
+#ifndef imELMODBUS_h
+#define imELMODBUS_h
+
+#include "imframe.h"
+#include "imdebug.h"
+
+#define IDD  0  
+
+#define TXEN 5
+#include <ModbusRtu.h>
+  // https://github.com/smarmengol/Modbus-Master-Slave-for-Arduino/blob/master/ModbusRtu.h#L1391
+
+
+// Setup a oneWire instance to communicate with ANY OneWire devices
+
+//SoftwareSerial wire(wireRX,wireTX);
+
+
+//HardwareSerial wire=Serial;
+
+
+Modbus master(IDD,Serial,TXEN);
+modbus_t telegram;
+
+uint16_t cpuVin;
+uint16_t cpuTemp;
+uint16_t cpuVinCycle=0;
+
+
+uint16_t au16data[32];
+uint8_t u8state;
+
+void SetupModbus()
+{
+  power_usart0_enable();
+  pinMode(0,INPUT);
+  pinMode(1,OUTPUT);
+  //Serial.begin(9600,SERIAL_8N2);//modbus
+//  Serial.begin(38400,SERIAL_8N1);//ascii
+ 
+Serial.begin(9600);
+master.setTimeOut(200);
+master.start();
+}
+
+
+void sendTelegram(){
+   telegram.u8id = 1; // slave address
+    telegram.u8fct = 3; // function code (this one is registers read)
+    telegram.u16RegAdd = 134; // start address in slave
+    telegram.u16CoilsNo = 1; // number of elements (coils or registers) to read
+    telegram.au16reg = au16data; // pointer to a memory array in the Arduino
+
+    master.query( telegram ); // send query (only once)
+}
+
+void PrepareModbus()
+{
+ // wire.write('ab');
+ /* 
+ Serial.write(7);
+ */
+  //Serial.write("TEMPTEST05E\r");
+ 
+ // Serial.write("\rTEMP06i\r");
+ // Serial.flush();
+  //wire.listen();
+  sendTelegram();
+}  
+
+void DataModbus(IMFrame &frame)
+{
+  if (cpuVinCycle % 28==0){ 
+    SetupADC();
+    cpuVin=internalVcc();
+    cpuTemp=internalTemp();
+    cpuTemp=internalTemp();
+    ShutOffADC();
+    power_adc_disable();
+  }
+  master.poll();
+   cpuVinCycle++;
+  
+   IMFrameData *data =frame.Data();
+
+   data->w[6]=trx.Connected();
+   data->w[1]=cpuTemp;
+   data->w[0]=cpuVin;
+   data->w[10]=0xA33A;
+   for (int i=0;i<6;i++){
+  //  if (Serial.available()>0) {
+  //    char inb=Serial.read();
+  //    data->w[i]=byte(inb);
+  //  }
+      data->w[i+2]=au16data[i];
+   }
+  
+  data->w[7]=master.getState();
+}
+
+
+
+
+
+#endif
+//
+// END OF FILE
+//
---
+++ modbus485/modbus485.ino
@@ -0,0 +1,172 @@
+
+#include <imframe.h>
+#include <imatmega.h>
+#include <EEPROM.h>
+#include <SPI.h>
+#include "imdebug.h"
+//#include "SoftwareSerial.h"
+
+/******************************** Configuration *************************************/
+
+// Data wire is plugged into pin 2 on the Arduino
+
+#define MMAC 0x630001  // My MAC
+#define ServerMAC 0xA0000  // Server  MAC
+#define MDEVICE 0x63     //Type of device
+#define MCHANNEL 2
+
+/************************* Module specyfic functions **********************/
+
+#include "imtrans.h"
+#include "imtimer.h"
+#include "imbufrfm69.h"
+
+Transceiver trx;
+IMBuffer    buffer;
+
+#include "modbus485.h"
+
+void PrepareData()
+{
+      if (trx.CycleData())
+      {
+        DBGLEDON();
+        PrepareModbus();
+        DBGLEDOFF();
+      }
+}  
+
+void SendData()
+{
+      if (trx.CycleData()) {
+        static IMFrame frame;
+        frame.Reset();
+        DataModbus(frame);
+        trx.Wakeup();
+        trx.SendData(frame);
+        trx.Transmit();
+      }
+}
+
+byte OrderData(uint16_t a){
+//  DBGLEDON();
+  return 1;
+}  
+
+
+#ifdef HAVE_HWSERIAL1
+  tttt
+#endif
+  
+void ReceiveData()
+{
+      while (trx.GetData())
+      {
+        if (trx.Parse())
+        {
+          DBGINFO(" rxGET ");
+        }
+      }
+}
+
+void PrintStatus()
+{
+  DBGINFO("\r\n");
+  DBGINFO(" Status ");
+//  trx.printStatus();
+  DBGINFO("\r\n");
+}
+
+
+void stageloop(byte stage)
+{
+  switch (stage)
+  {
+    case STARTBROADCAST: trx.Knock();      break;
+    case STOPBROADCAST:    trx.StopListenBroadcast();PrepareData();    break;
+    case STARTDATA: SendData();  /*SendDataFlood();*/break;
+    case STOPDATA:   trx.StopListen();      break;
+    case LISTENDATA : ReceiveData();break;
+    case LISTENBROADCAST : ReceiveData();break;
+    case IMTimer::IDDLESTAGE : {        ReceiveData();break; }
+    case IMTimer::PERIOD : 
+  //      ERRFLASH();
+  //     PrintStatus();
+    break;
+    default:
+    break;
+  }
+}
+
+
+
+
+
+void setup()
+{
+  resetPin();
+  #ifdef DBGCLOCK
+    pinMode(DBGCLOCK,OUTPUT);
+    digitalWrite(DBGCLOCK ,HIGH);
+  #endif
+  pinMode(10,OUTPUT);
+  digitalWrite(10,HIGH);
+  DBGPINHIGH();
+  DBGPINLOW();
+  INITDBG();
+  setupTimer2();
+  power_timer0_enable();
+  SetupADC();
+  wdt_enable(WDTO_8S);
+  interrupts();
+ 
+ //   SetupELKT();
+ //  disableADCB();
+  SetupModbus();
+  trx.myMAC=MMAC;
+  //trx.startMAC=0;
+  trx.serverMAC=ServerMAC;
+  trx.myChannel=MCHANNEL;
+  trx.myDevice=MDEVICE;
+//  trx.funOrder=&OrderData;
+  trx.Init(buffer);
+//  trx.timer.onStage=stageloop;
+
+
+#if DBGLED>=1
+  if (true){
+    DBGLEDON();
+    delaySleepT2(300);
+    DBGLEDOFF();
+   } else{
+    DBGLEDON();
+    delay(200);
+    DBGLEDOFF();
+    delay(200);
+    DBGLEDON();
+    delay(200);
+    DBGLEDOFF();
+    delay(200);
+    DBGLEDON();
+    delay(200);
+    //reboot();
+  }
+#endif
+ // power_timer0_disable();
+  setupTimer2();
+    power_usart0_enable();
+
+}
+
+void loop()
+{
+  wdt_reset();
+//  PrintStatus(); 
+  byte xstage;
+  do{
+     xstage=trx.timer.WaitStage();
+    // DBGPINLOW();
+     stageloop(xstage);
+   // DBGPINHIGH();
+  }while( xstage!=IMTimer::PERIOD);
+}
